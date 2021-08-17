318d6d576d72c3723cc97d5b6023666236d973ea
PhillipMcDonald <81326101+PhillipMcDonald@users.noreply.github.com>
2021-08-17 13:25:58 -0400

    Added code for payment options as well as form validation
    Added code to show appropriate payment method information for selected input, Started adding form validation using regular expressions to validate whether user input meets the pre set criteria.

--- js/script.js
+++ js/script.js
@@ -19,13 +19,13 @@ if($('#title option:selected').val()==='other'){
 
 })
 
+
+
+
+
 // disabled the "color" dropdown menu
 
 $('#color').prop("disabled", true)
-
-
-
-
 
 const color = $('#color')
 
@@ -76,5 +76,69 @@ $('#activities').on('change', function(activity){
 
 })
 
+/////////////////////////////////////////////////////
 
+$('#paypal').hide();
+
+$('#bitcoin').hide();
+
+$('#payment option[value = "credit-card"]').attr('selected','selected');
+
+$('#payment').on('change', function(paymentOption){
+  
+let paymentOptionSelected = paymentOption.target.value;
+
+if(paymentOptionSelected === 'paypal'){
+  $('#paypal').show();
+  $('#credit-card').hide();
+  $('#bitcoin').hide();
+
+} else if(paymentOptionSelected === 'bitcoin'){
+  $('#bitcoin').show();
+  $('#credit-card').hide();
+  $('#paypal').hide();
+
+}else if(paymentOptionSelected === 'credit-card'){
+  $('#credit-card').show();
+  $('#bitcoin').hide();
+  $('#paypal').hide();
+}
+
+});
+
+//////////////////////////////////////////////////////
+
+$("form").on('submit', function(event){
+  
+  let nameEntered = $('#name').val();
+  let emailEntered = $('#email').val();
+  
+  const nameTest = /[\w\W]/.test(nameEntered);
+  const emailTest = /^[^@]+@[^@.]+\.[a-z]+$/gim.test(emailEntered);
+  
+  
+
+
+  if (nameTest !== true){
+
+    
+    $('#name-hint').show();
+    $('#name').addClass("not-valid error border");
+    event.preventDefault();
+    
+  }
+  
+
+  if (emailTest !== true){
+
+    
+    $('#email-hint').show();
+    $('#email').addClass("not-valid error border");
+    event.preventDefault();
+    
+  }
+
+
+
+})
 
