94f7abe4a7f92f15a09b2a1cff912337910933bc
PhillipMcDonald <81326101+PhillipMcDonald@users.noreply.github.com>
2021-08-17 14:13:07 -0400

    Finished adding form validation
    finished adding code that tests whether input provided matches the testing criteria.  If it does, the class of "valid" is added and a checkmark appears
    
    if the input if not valid, an error border and icon are added instead.

--- js/script.js
+++ js/script.js
@@ -112,28 +112,89 @@ $("form").on('submit', function(event){
   
   let nameEntered = $('#name').val();
   let emailEntered = $('#email').val();
+  let ccEntered = $('#cc-num').val()
+  let zipEntered = $('#zip').val();
+  let cvvEntered = $('#cvv').val();
   
   const nameTest = /[\w\W]/.test(nameEntered);
   const emailTest = /^[^@]+@[^@.]+\.[a-z]+$/gim.test(emailEntered);
-  
-  
+  const ccTest = /^[0-9]{13,16}$/.test(ccEntered)
+  const zipTest = /^[0-9]{5}$/.test(zipEntered)
+  const cvvTest = /^[0-9]{3}$/.test(cvvEntered)
 
 
-  if (nameTest !== true){
+  if(nameTest=== true){
+
+    $("[for=name]").addClass('valid')
+
+  }
+
+ else if (nameTest !== true){
 
     
     $('#name-hint').show();
-    $('#name').addClass("not-valid error border");
+    $("[for=name]").addClass("not-valid error border");
     event.preventDefault();
     
   }
   
 
-  if (emailTest !== true){
+  if(emailTest=== true){
+
+    $("[for=email]").addClass('valid')
+
+  }
+
+ else if (emailTest !== true){
 
     
     $('#email-hint').show();
-    $('#email').addClass("not-valid error border");
+    $("[for=email]").addClass("not-valid error border");
+    event.preventDefault();
+    
+  }
+
+
+
+  if(ccTest=== true){
+
+    $("[for=cc-num]").addClass('valid')
+
+  } else if (ccTest !== true){
+
+    
+    $('#cc-hint').show();
+    $("[for=cc-num]").addClass("not-valid error border");
+    event.preventDefault();
+    
+
+  }
+
+  if (zipTest === true){
+
+
+    $('[for=zip]').addClass("valid");
+    
+  }else if (zipTest !== true) {
+
+  
+    $('#zip-hint').show();
+    $('[for=zip]').addClass("not-valid error border");
+    event.preventDefault();
+  }
+
+
+
+  if (cvvTest === true){
+
+
+    $('[for=cvv]').addClass("valid");
+    
+  }else if (cvvTest !== true){
+
+    
+    $('#cvv-hint').show();
+    $('[for=cvv]').addClass("not-valid error border");
     event.preventDefault();
     
   }
